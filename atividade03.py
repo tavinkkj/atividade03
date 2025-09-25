7f3bbb1d4f79018c9d0c12a90763bb11525a2531
otavio <otavio.olivindo@aluno.ce.gov.br>
2025-09-25 14:04:58 -0300

    Add files via upload

---
+++ atividade03.py
@@ -0,0 +1,14 @@
+# Atividade 43– Tipos de Dados
+
+# Crie 4 variáveis:
+# Um número inteiro
+# Um número decimal
+# Um texto
+# Um valor lógico (True ou False)
+
+# Imprima todas elas no console.
+numero_inteiro = 24
+numero_decimal = 9.9
+texto = "dadson quatiguabense"
+valor_logico = False
+print(f'número inteiro: {numero_inteiro}, numero decimal: {numero_decimal}, texto: {texto} e o valor lógico: {valor_logico}.')
